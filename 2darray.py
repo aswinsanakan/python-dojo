51d5ab8d708401b8618e701bcc6f211e5c73d1d3
aswinsanakan <aswinsanakan@gmail.com>
2017-10-31 15:50:37 +0530

    Add numpy program

---
+++ 2darray.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+pop = [2.5, 2.9, 3.5, 4.3, 5.2, 5.9, 6.3, 6.5, 6.7, 6.9]
+
+# Create a numpy array
+np_pop = np.array(pop)
+
+print(pop)
+
+# Multiply all elements by 2
+np_pop = np_pop * 2
+
+print(np_pop)
